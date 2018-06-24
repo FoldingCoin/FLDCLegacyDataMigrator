e545d0ae166d2c9dfa82d1672ec6b8a039313e55
ParkerMB <ParkerMB@DESKTOP-EQ8VGPI>
2018-06-23 22:39:39 -0400

    Create solution skeleton.

---
+++ FLDCLegacyDataMigrator/FLDCLegacyDataMigrator/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FLDCLegacyDataMigrator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
